f2ff4ea26a6345d9773066e5cec336959a62f8f1
Gribov Pavel <donpadlo@gmail.com>
2020-09-29 20:44:06 +0300

    делаю каркас

--- public/index.php
+++ public/index.php
@@ -5,5 +5,13 @@
 # Изначальный автор: Грибов Павел
 # https://грибовы.рф
 
-require_once '../config.php';  // основные настройки
-require_once '../vendor/autoload.php';
+define('WUO_ROOT', dirname(__FILE__));
+
+$time_start = microtime(true); // Засекаем время начала выполнения скрипта
+
+header('Content-Type: text/html; charset=utf-8');
+
+require_once WUO_ROOT.'/../config.php';  // основные настройки
+require_once WUO_ROOT.'/../vendor/autoload.php';
+
+require_once WUO_ROOT.'/../class/pdo_connect.php'; // соединяемся с БД
